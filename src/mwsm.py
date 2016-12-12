0eca46de20c41dc64504b25cc48a14940bf31072
dkrukov <dan.krukov@softwareag.com>
2016-12-11 22:38:27 -0700

    small tweaks

--- src/mwsm.py
+++ src/mwsm.py
@@ -6,11 +6,10 @@ from mws import res #mws page resources, links etc
 
 def login():
     if g.driver is None: prep()
-    tc('get url')
+    tc('get url '+g.cfg['url'])
     g.driver.get(g.cfg['url'])
-    tc('login page')
+    tc('login '+g.cfg['username']+'/'+g.cfg['password']) 
     g.driver.find_element_by_name('username').send_keys(g.cfg['username'] + Keys.RETURN)
-    tc('login')
     g.driver.find_element_by_name('password').send_keys(g.cfg['password'] + Keys.RETURN)
     x="//*/a[contains (@title, 'Logout')]"; g.wait.until(EC.element_to_be_clickable((By.XPATH, x)))
 
