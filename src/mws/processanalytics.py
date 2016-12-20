705a8a53f9c648baed6717f96332edab69494ead
dkrukov <dan.krukov@softwareag.com>
2016-12-20 15:44:24 -0700

    fix proc select

--- src/mws/processanalytics.py
+++ src/mws/processanalytics.py
@@ -1,34 +1,45 @@
 import g #globs: driver, wait...
 from g import tc
 EC=g.EC; By=g.By; Keys=g.Keys #selenium statics
+from selenium.webdriver.support.select import Select
+from time import sleep
 
 
 def selectprocess (p):
-	"""
-	select a process from dropdown
-	"""
-	tc('select '+p)
-	x="//*/select[contains (@name, 'processSelectListbox')]"; g.wait.until(EC.element_to_be_clickable((By.XPATH, x))).send_keys(p)
-	t=g.wait.until(EC.element_to_be_clickable((By.XPATH, x))).text
-	print(t)
-	pass
-	
-def	selectrange(p):
-	"""
-	select a range from dropdown
-	"""
-	pass
+    """
+    select a process from dropdown
+    """
+    tc('select '+p)
+    x="//*/select[contains (@name, 'processSelectListbox')]"
+    e=g.wait.until(EC.element_to_be_clickable((By.XPATH, x)))
+    select=Select(e)
+    select.select_by_visible_text(p)
+    sleep(2) #ugly page mutation
+    e=g.wait.until(EC.element_to_be_clickable((By.XPATH, x)))
+    select=Select(e)
+    if select.first_selected_option.text != p:
+        tc('','fail')
+    #for opt in select.all_selected_options:
+    #    print(opt.text)
+    #for opt in select.options:
+    #    print(opt.text)
 
-def	selectvolume(p):
-	"""
-	select a range from dropdown
-	"""
-	tc('select volume '+p)
-	pass
+def selectrange(p):
+    """
+    select a range from dropdown
+    """
+    pass
 
-def	nav(p):
-	"""
-	click on Process Instance ID link
-	"""
-	pass
+def selectvolume(p):
+    """
+    select a range from dropdown
+    """
+    tc('select volume '+p)
+    pass
+
+def nav(p):
+    """
+    click on Process Instance ID link
+    """
+    pass
 
