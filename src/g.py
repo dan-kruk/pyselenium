4f9f0a6917f3343917f21bd24c818d61d94d9278
dkrukov <dan.krukov@softwareag.com>
2016-12-21 21:26:27 -0700

    junit classname, trace detals tweaks

--- src/g.py
+++ src/g.py
@@ -119,7 +119,7 @@ def logjunit(name, status, time):
     #print (inspect.stack())
     if status in ['fail','fatal']:
         s='<failure message="'+status+' level error" type="reserved">\n'
-        s+='trace details\n'
+        s+='trace details...\nscreenshot '+LOGS+'/screenshot-'+re.sub('[^a-zA-Z0-9-]',"_",tc_name)+'.png'+'\n'
         s+='</failure>\n'
-    junitfile.write('<testcase classname="'+str(os.getpid())+'-'+MODULE+'" name="'+name+'" time="'+str(time)+'">\n'+s+'</testcase>\n')
+    junitfile.write('<testcase classname="'+MODULE+'.'+str(os.getpid())+'" name="'+name+'" time="'+str(time)+'">\n'+s+'</testcase>\n')
 
