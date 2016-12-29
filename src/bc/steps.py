812ed51e24230a14d17410b28519c338b0cca5e6
dkrukov <dan.krukov@softwareag.com>
2016-12-29 14:52:57 -0700

    fix bc window switch thing

--- src/bc/steps.py
+++ src/bc/steps.py
@@ -4,7 +4,13 @@ EC=g.EC; By=g.By; Keys=g.Keys
 from time import sleep
 
 def check():
+
     tc('bc step summary table displayed')
-    x="//*[@id='stepSummaryTable']"
-    sleep(3)
-    e=g.wait.until(EC.presence_of_element_located((By.XPATH, x)))
+    g.driver.switch_to_window(g.driver.window_handles[1]) #bc is an alt window
+
+    x=".//*[@id='stepSummaryTable']"; e=g.wait.until(EC.presence_of_element_located((By.XPATH, x)))
+
+    #tc('flick the movie')
+    #for i in range(1,1001):
+    #    g.driver.switch_to_window(g.driver.window_handles[i%2]) #bc is an alt window
+
