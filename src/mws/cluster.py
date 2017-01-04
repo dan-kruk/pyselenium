e45897efdc7faaf857ea5d700813af2be08e8102
dkrukov <dan.krukov@softwareag.com>
2017-01-04 14:46:57 -0700

    fix chroot issue with prior feurl change

--- src/mws/cluster.py
+++ src/mws/cluster.py
@@ -9,7 +9,7 @@ import re
 mws cluster settings
 """
 
-def chroot (d='alt',set_feurl=False):
+def chroot (d='alt',feurl=False):
     """
     create/update/clear mws cluster root context and optionally front end url; d='' - clears context
     """
@@ -17,14 +17,15 @@ def chroot (d='alt',set_feurl=False):
     x=".//*[contains (@id, 'clusteredSetupPanel')]"; e = g.wait.until(EC.element_to_be_clickable((By.XPATH, x))); e.click()
     sleep(1) #dom mutations TODO fix
     x=".//*[contains (@id, 'clusterRootcontext')]"; e = g.wait.until(EC.element_to_be_clickable((By.XPATH, x)))
-    if set_feurl: _chfeurl()
+    e.clear(); e.send_keys(d)
+    if feurl: chfeurl(d)
     tc('submit and check changes')        
     x=".//*[contains (@id, 'submitButton')]"; g.driver.find_element(By.XPATH, x).send_keys(Keys.RETURN)
     x=".//*[text()='Changes to Cluster Node roles or ports are only effective after the Node restart']"; e = g.wait.until(EC.element_to_be_clickable((By.XPATH, x)))
     x=".//*[contains (@id, 'standaloneSetupPanel')]"; e = g.wait.until(EC.element_to_be_clickable((By.XPATH, x))); e.click()
     sleep(1) #dom mutations TODO fix
 
-def _chfeurl():
+def chfeurl(d):
     """
     change front end url
     normally don't do it unless you bounce mws right after that or UI may end up inaccessible
@@ -37,5 +38,4 @@ def _chfeurl():
         fe=''.join(fe[0:3])+d #compose url
         tc('change feurl to '+fe)
         e1.clear(); e1.send_keys(fe)
-    e.clear(); e.send_keys(d)
 
