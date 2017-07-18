322fb8097bce769b041c2abf257e127d5314289f
dkrukov <dan.krukov@softwareag.com>
2017-07-18 10:19:21 -0600

    GB - add more fuzzy expected rule error counts

--- t/dbo_optimize_integration/problem_kpi.py
+++ t/dbo_optimize_integration/problem_kpi.py
@@ -18,7 +18,7 @@ try:
 
     ui.navauth('Problems')
     rules=['DBO_Process- Absolute Step Timeout Process','DBO_Process- Step Timeout Process','DBO order_amount by customer (KPI)']
-    errors=[{'Proc':'3','Step':'0','Stage':'3','Rule':'4 5'},{'Proc':'0','Step':'1','Stage':'3','Rule':'1 2 3'},{'Proc':'0','Step':'0','Stage':'3','Rule':'1 2'}]
+    errors=[{'Proc':'3','Step':'0','Stage':'3','Rule':'3 4 5'},{'Proc':'0','Step':'1','Stage':'3','Rule':'1 2 3 4'},{'Proc':'0','Step':'0','Stage':'3','Rule':'1 2'}]
 
     for r in range(0,len(rules)):
         ui.search(rules[r])
