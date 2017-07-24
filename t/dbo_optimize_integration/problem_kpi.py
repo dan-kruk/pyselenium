c873096de95cf7c11e0618247dae941085e6e5a0
dkrukov <dan.krukov@softwareag.com>
2017-07-24 10:38:36 -0600

    extend exp result on bc->errors->RRule count

--- t/dbo_optimize_integration/problem_kpi.py
+++ t/dbo_optimize_integration/problem_kpi.py
@@ -19,7 +19,7 @@ try:
 
     ui.navauth('Problems')
     rules=['DBO_Process- Absolute Step Timeout Process','DBO_Process- Step Timeout Process','DBO order_amount by customer (KPI) pubDocDBO/customer.pubDocDBO/customer.borotuga']
-    errors=[{'Proc':'3','Step':'0','Stage':'3','Rule':'3 5'},{'Proc':'0 1','Step':'1','Stage':'3','Rule':'1 2 3'},{'Proc':'0 1','Step':'0','Stage':'3','Rule':'2'}]
+    errors=[{'Proc':'3','Step':'0','Stage':'3','Rule':'3 5'},{'Proc':'0 1','Step':'1','Stage':'3','Rule':'1 2 3 4'},{'Proc':'0 1','Step':'0','Stage':'3','Rule':'2'}]
 
     for r in range(0,len(rules)):
         ui.search('"'+rules[r]+'"') #search quoted(exact in mws)
